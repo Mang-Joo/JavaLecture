6925b853397ffa2cb8e4c9f26fe4c4f202f881d3
KimWanJoo <wjwan0915@gmail.com>
2022-07-15 23:08:41 +0900

    feat: 익명 클래스

--- src/main/java/LocalPapers.java
+++ src/main/java/LocalPapers.java
@@ -9,13 +9,32 @@ public class LocalPapers {
     }
 
     public Printable getPrinter() {
+        /*
+        * 인터페이스의 구현 결과 하나밖에 없다.
+        * */
         class Printer implements Printable {
             @Override
             public void print() {
                 System.out.println("con = " + con);
             }
         }
-        return new Printer();
+        /*
+        * return new Printable(); 추상 메서드이기 때문에 인스턴스 생성이 안된다.
+        * print라는 메서드를 구현해야 한다. 그것을 한번에 처리한다.
+        * */
+//        return new Printer();
+
+        /*
+        * Printable 인터페이스를 구현한 인스턴스를 return
+        * 이름이 없는 익명 클래스!
+         * */
+        return new Printable() {
+            @Override
+            public void print() {
+                System.out.println("con = " + con);
+            }
+        };
+
     }
 
     public static void main(String[] args) {
